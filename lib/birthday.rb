c89780771307e1759aa715f3d23e4747a1e7a541
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-07 14:01:55 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,16 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |key ,value|
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
+  end
+  
 end
-
+def age_appropriate_birthday(birthday_kids)
+    birthday_kids.each do |key ,value|
+      if birthday_kids[key] == 12 || birthday_kids[key] < 12
+        puts "Happy Birthday #{key}! You are now #{value} years old!"
+      end
+    end
+  end
 
